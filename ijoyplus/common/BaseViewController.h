df00b09311ca949d39c563b237309ffaaefebc27
Steven Zhang <hmilvu@gmail.com>
2012-09-13 11:48:58 +0800

    UI

---
+++ ijoyplus/common/BaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  BaseViewController.h
+//  ijoyplus
+//
+//  Created by joyplus1 on 12-9-13.
+//  Copyright (c) 2012年 joyplus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseViewController : UIViewController
+
+@end
---
+++ ijoyplus/common/BaseViewController.m
@@ -0,0 +1,43 @@
+//
+//  BaseViewController.m
+//  ijoyplus
+//
+//  Created by joyplus1 on 12-9-13.
+//  Copyright (c) 2012年 joyplus. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface BaseViewController ()
+
+@end
+
+@implementation BaseViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
