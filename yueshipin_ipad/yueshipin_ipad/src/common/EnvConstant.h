72978c7c6da787850459cb0c24b4b6cc309e84a6
Steven Zhang <hmilvu@gmail.com>
2013-03-29 21:12:42 +0800

    0.9.9

--- yueshipin_ipad/yueshipin_ipad/src/common/EnvConstant.h
+++ yueshipin_ipad/yueshipin_ipad/src/common/EnvConstant.h
@@ -9,8 +9,8 @@
 #import <Foundation/Foundation.h>
 
  //0: 测试环境     1. 正式环境
-#define ENVIRONMENT 0
-#define VERSION @"0.9.9alpha"
+#define ENVIRONMENT 1
+#define VERSION @"0.9.9"
 
 //0: 关闭loglog      1. 打开log      Log功能只有再测试环境才生效
 #define LOG_ENABLED 0
