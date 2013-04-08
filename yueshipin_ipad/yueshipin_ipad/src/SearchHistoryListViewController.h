c37de531ec36ab944751a2c5eca6c0df89692f95
Steven Zhang <hmilvu@gmail.com>
2013-04-08 15:59:08 +0800

    new UI

---
+++ yueshipin_ipad/yueshipin_ipad/src/SearchHistoryListViewController.h
@@ -0,0 +1,13 @@
+//
+//  SearchHistoryListViewController.h
+//  yueshipin
+//
+//  Created by joyplus1 on 13-4-8.
+//  Copyright (c) 2013年 joyplus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchHistoryListViewController : UITableViewController
+
+@end
