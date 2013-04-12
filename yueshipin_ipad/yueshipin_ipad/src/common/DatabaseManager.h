b99b595eebcc928b279580adbd8edcb80fb8869e
Steven Zhang <hmilvu@gmail.com>
2013-04-12 13:29:05 +0800

    change sqllite

---
+++ yueshipin_ipad/yueshipin_ipad/src/common/DatabaseManager.h
@@ -0,0 +1,24 @@
+//
+//  DatabaseManager.h
+//  yueshipin
+//
+//  Created by joyplus1 on 13-4-12.
+//  Copyright (c) 2013年 joyplus. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "DatabaseObject.h"
+
+@interface DatabaseManager : NSObject
+
++ (id)databaseQueueWithPath:(NSString*)aPath;
+
+- (NSArray *)findByCriteria:(Class)dbObjectClass queryString:(NSString *)queryString;
+- (DatabaseObject *)findFirstByCriteria:(Class *)dbObjectClass queryString:(NSString *)queryString;
+- (NSArray *)allObjects:(Class *)dbObjectClass;
+- (double)performSQLAggregation: (NSString *)query;
+- (void)deleteObject:(DatabaseObject *)dbObject;
+- (void)save:(DatabaseObject *)dbObject;
+- (NSInteger)count:(Class*)dbObjectClass;
+- (NSInteger)countByCriteria:(Class *)dbObjectClass queryString:(NSString *)queryString;
+@end
---
+++ yueshipin_ipad/yueshipin_ipad/src/common/DatabaseManager.m
@@ -0,0 +1,92 @@
+//
+//  DatabaseManager.m
+//  yueshipin
+//
+//  Created by joyplus1 on 13-4-12.
+//  Copyright (c) 2013年 joyplus. All rights reserved.
+//
+
+#import "DatabaseManager.h"
+#import "FMDatabaseQueue.h"
+#import "FMDatabase.h"
+#import "DownloadItem.h"
+#import "SubdownloadItem.h"
+#import "SegmentUrl.h"
+
+@interface DatabaseManager (){
+    NSString *path;
+}
+
+@end
+
+@implementation DatabaseManager
+
++ (id)databaseQueueWithPath:(NSString*)aPath
+{
+    DatabaseManager *dbManager = [[DatabaseManager alloc]init];
+    [dbManager setPath:aPath];
+    return dbManager;
+}
+
+- (void)setPath:(NSString*)aPath {
+    path = aPath;
+}
+
+- (NSArray *)findByCriteria:(Class)dbObjectClass queryString:(NSString *)queryString
+{
+    FMDatabase *db = [FMDatabase databaseWithPath:path];
+    if (![db open]) {
+        NSLog(@"Could not open db in DatabaseManager!");
+        return nil;
+    }
+    FMResultSet *rs = [db executeQuery:queryString];
+    NSMutableArray *resultArray = [[NSMutableArray alloc]initWithCapacity:5];
+    while ([rs next]) {
+        if (dbObjectClass == DownloadItem.class) {
+            DownloadItem *tempDbObj = [[DownloadItem alloc]init];
+            tempDbObj.name = [rs stringForColumn:@"name"];
+            [resultArray addObject:tempDbObj];
+        } else if (dbObjectClass == DownloadItem.class) {
+            SubdownloadItem *tempDbObj = [[SubdownloadItem alloc]init];
+            tempDbObj.name = [rs stringForColumn:@"name"];
+        } else if (dbObjectClass == SegmentUrl.class) {
+            SegmentUrl *tempDbObj = [[SegmentUrl alloc]init];
+            tempDbObj.url = [rs stringForColumn:@"url"];
+        }
+    }
+    [rs close];
+    [db close];
+    return  resultArray;
+}
+
+- (DatabaseObject *)findFirstByCriteria:(Class *)dbObjectClass queryString:(NSString *)queryString
+{
+    
+}
+
+- (NSArray *)allObjects:(Class *)dbObjectClass
+{
+    
+}
+- (double)performSQLAggregation: (NSString *)query
+{
+    
+}
+- (void)deleteObject:(DatabaseObject *)dbObject
+{
+    
+}
+- (void)save:(DatabaseObject *)dbObject
+{
+    
+}
+- (NSInteger)count:(Class *)dbObjectClass
+{
+    
+}
+- (NSInteger)countByCriteria:(Class *)dbObjectClass queryString:(NSString *)queryString
+{
+    
+}
+
+@end
