99af0c33082330164d2ea0c83d275095a4b0a1ea
Steven Zhang <hmilvu@gmail.com>
2013-05-09 12:35:09 +0800

    weixin env

--- theatre_iphone/yueshipin_ipad/src/common/EnvConstant.h
+++ theatre_iphone/yueshipin_ipad/src/common/EnvConstant.h
@@ -40,6 +40,7 @@
 //正式环境
 #if ENVIRONMENT   
     #define kDefaultAppKey @"ijoyplus_ios_001"
+    #define kDefaultCheckBindAppKey @"ijoyplus_android_0001bj"
     #define PARSE_APP_ID @"UBgv7IjGR8i6AN0nS4diS48oQTk6YErFi3LrjK4P"
     #define PARSE_CLIENT_KEY @"Y2lKxqco7mN3qBmZ05S8jxSP8nhN92hSN4OHDZR8"
     #define kABaseURLString @"http://api.joyplus.tv/"
@@ -48,6 +49,7 @@
 //测试环境
 #else
     #define kDefaultAppKey @"ijoyplus_ios_001bj"
+    #define kDefaultCheckBindAppKey @"ijoyplus_android_0001bj"
     #define PARSE_APP_ID @"5FNbLx7dnRAx3knxV4rOdaLMRJMByqfKjWQRQakT"
     #define PARSE_CLIENT_KEY @"RZHrZVn6MK8VGZxfpeshrC2tpxpzzMOZjU0rSS6X"
     #define kABaseURLString @"http://apitest.yue001.com/"
