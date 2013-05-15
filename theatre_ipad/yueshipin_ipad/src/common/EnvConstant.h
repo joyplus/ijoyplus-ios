5c37271abca2c2cc9316521103a873af418b4f68
Steven Zhang <hmilvu@gmail.com>
2013-05-15 14:59:42 +0800

    ipad

--- theatre_ipad/yueshipin_ipad/src/common/EnvConstant.h
+++ theatre_ipad/yueshipin_ipad/src/common/EnvConstant.h
@@ -9,9 +9,9 @@
 #import <Foundation/Foundation.h>
 
  //0: 测试环境     1. 正式环境
-#define ENVIRONMENT 1
+#define ENVIRONMENT 0
 
-#define VERSION @"1.0.0"
+#define VERSION @"1.0.1alpha"
 
 //0: 关闭loglog      1. 打开log      Log功能只有再测试环境才生效
 #define LOG_ENABLED 0
@@ -33,7 +33,7 @@
 
 
 //友盟在线参数
-#define SHOW_VIDEO_SWITCH @"showVideoSwitch1"
+#define SHOW_VIDEO_SWITCH @"showVideoSwitch2"
 #define CLOSE_VIDEO_MODE @"closeVideoMode"
 
 
