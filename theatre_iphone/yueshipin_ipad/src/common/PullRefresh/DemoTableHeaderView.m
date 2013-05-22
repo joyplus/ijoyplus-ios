d28105d6b571f97f19ee49f41f0b12cf34fadeca
吴双熔 <shuangrong.wu@joyplus.tv>
2013-05-22 04:19:15 -0700

    bug fix

--- theatre_iphone/yueshipin_ipad/src/common/PullRefresh/DemoTableHeaderView.m
+++ theatre_iphone/yueshipin_ipad/src/common/PullRefresh/DemoTableHeaderView.m
@@ -17,10 +17,14 @@
 - (void) awakeFromNib
 {
   self.backgroundColor = [UIColor clearColor];
-  title.font = [UIFont boldSystemFontOfSize:15];
-  title.textColor = [UIColor grayColor];
-  dateLabel.font = [UIFont boldSystemFontOfSize:11];
-  dateLabel.textColor = [UIColor grayColor];
+  title.font = [UIFont boldSystemFontOfSize:13];
+  title.textColor = [UIColor colorWithRed:153/255.0 green:153/255.0 blue:153/255.0 alpha:1.0];
+  title.shadowColor = [UIColor colorWithWhite:0.9f alpha:1.0f];
+  title.shadowOffset = CGSizeMake(0.0f, 1.0f);
+  dateLabel.font = [UIFont boldSystemFontOfSize:12];
+  dateLabel.textColor = [UIColor colorWithRed:153/255.0 green:153/255.0 blue:153/255.0 alpha:1.0];
+  dateLabel.shadowColor = [UIColor colorWithWhite:0.9f alpha:1.0f];
+  dateLabel.shadowOffset = CGSizeMake(0.0f, 1.0f);
   dateLabel.text = @"";
   [super awakeFromNib];
 }
