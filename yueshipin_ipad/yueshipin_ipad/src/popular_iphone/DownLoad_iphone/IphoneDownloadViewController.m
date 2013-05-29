04ff92c9218e6e044fc6bcd010be5ade15d8330d
Steven Zhang <hmilvu@gmail.com>
2013-05-29 10:58:28 +0800

    bug  fix

--- yueshipin_ipad/yueshipin_ipad/src/popular_iphone/DownLoad_iphone/IphoneDownloadViewController.m
+++ yueshipin_ipad/yueshipin_ipad/src/popular_iphone/DownLoad_iphone/IphoneDownloadViewController.m
@@ -459,6 +459,7 @@
                 iphoneAVPlayerViewController.nameStr = item.name;
                 iphoneAVPlayerViewController.prodId = item.itemId;
                 iphoneAVPlayerViewController.playNum = 1;
+                iphoneAVPlayerViewController.videoType = 1;
                 [self presentViewController:iphoneAVPlayerViewController animated:YES completion:nil];
             }
             else{
