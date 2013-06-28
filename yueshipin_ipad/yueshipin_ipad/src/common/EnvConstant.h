d08f6d87aa2af5777956c389e76821adac752df2
Steven Zhang <hmilvu@gmail.com>
2013-06-28 14:27:06 +0800

    env

--- yueshipin_ipad/yueshipin_ipad/src/common/EnvConstant.h
+++ yueshipin_ipad/yueshipin_ipad/src/common/EnvConstant.h
@@ -9,7 +9,7 @@
 #import <Foundation/Foundation.h>
 
  //0: 测试环境     1. 正式环境
-#define ENVIRONMENT 1
+#define ENVIRONMENT 0
 #define AMERICANVIDEOS @"0"  //1-隐藏美剧 0-显示美剧
 
 #define VERSION @"1.0.3alpha"
@@ -33,9 +33,9 @@
 //#define CHANNEL_ID @"b006001"
 
 //友盟在线参数
-#define SHOW_VIDEO_SWITCH @"showVideoSwitch7"
-#define CLOSE_VIDEO_MODE @"closeVideoMode7"
-#define RECOMMEND_APP_SWITCH @"recommendAppSwitch7"
+#define SHOW_VIDEO_SWITCH @"showVideoSwitch8"
+#define CLOSE_VIDEO_MODE @"closeVideoMode8"
+#define RECOMMEND_APP_SWITCH @"recommendAppSwitch8"
 
 //正式环境
 #if ENVIRONMENT   
