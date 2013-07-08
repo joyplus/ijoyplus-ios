403ad0b0f18c0ff0713a68e6111d472835ec4a4a
lily <lily@jotekiiMac.local>
2013-07-08 16:13:20 +0800

    推动控制

--- theatre_iphone/yueshipin_ipad/src/popular_iphone/IphoneAVPlayerViewController.m
+++ theatre_iphone/yueshipin_ipad/src/popular_iphone/IphoneAVPlayerViewController.m
@@ -1455,6 +1455,11 @@ NSComparator cmptr2 = ^(NSString *obj1, NSString * obj2){
     tapGesture.numberOfTapsRequired = 1;
     tapGesture.numberOfTouchesRequired = 1;
     [avplayerView_ addGestureRecognizer:tapGesture];
+    
+    UIPanGestureRecognizer *panGesture = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(panAction:)];
+    panGesture.delegate = self;
+    [avplayerView_ addGestureRecognizer:panGesture];
+    
     [self.view addSubview:avplayerView_];
     
     ariplayView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"airplay_bg.png"]];
@@ -2218,6 +2223,54 @@ NSComparator cmptr2 = ^(NSString *obj1, NSString * obj2){
     }
 
 }
+
+-(void)panAction:(UIPanGestureRecognizer *)pan{//左右拖动设置进度，上下推动设置音量；
+    CGPoint offset = [pan translationInView:avplayerView_];
+    switch (pan.state) {
+        case UIGestureRecognizerStateBegan:{
+            if (fabs(offset.x)>=fabs(offset.y)){
+                [self beginScrubbing:mScrubber];
+            }
+            break;
+        }
+        case UIGestureRecognizerStateChanged:
+            if (fabs(offset.x)>=fabs(offset.y)) {//左右拖动
+                float nowValue = mScrubber.value;
+                nowValue = nowValue + 0.05*offset.x/kFullWindowHeight;
+                if (nowValue > 1) {
+                    nowValue = 1;
+                }
+                else if (nowValue < 0){
+                    nowValue = 0;
+                }
+                mScrubber.value = nowValue;
+                [self scrub:mScrubber];
+            }
+            else{//上下拖动
+                float nowValue = [MPMusicPlayerController applicationMusicPlayer].volume;
+                nowValue = nowValue - 0.05*offset.y/320;
+                if (nowValue > 1) {
+                    nowValue = 1;
+                }
+                else if (nowValue < 0){
+                    nowValue = 0;
+                }
+                [MPMusicPlayerController applicationMusicPlayer].volume = nowValue;
+            }
+            break;
+        case UIGestureRecognizerStateEnded:{
+            if (fabs(offset.x)>=fabs(offset.y)){
+                [self endScrubbing:mScrubber];
+            }
+            break;
+        }
+        default:
+            break;
+    }
+    
+    
+}
+
 - (void)clearPlayerData
 {
     [[UIApplication sharedApplication] setStatusBarHidden:NO];
